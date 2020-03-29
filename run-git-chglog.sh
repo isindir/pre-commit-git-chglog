1cd7abe56741f49f11f3cc3c48c3e99642eb252f
Eriks Zelenka <isindir@users.sf.net>
2020-03-29 17:13:44 +0100

    Fix

--- run-git-chglog.sh
+++ run-git-chglog.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
 
-failed=false
-
 git-chglog -o CHANGELOG.md
-if [[ $? -ne 0 ]]; then
-    failed=true
-fi
 
-#git diff --quiet
-#if [[ $? -ne 0 ]]; then
-#    failed=true
-#fi
-#
-#git diff --staged --quiet
-#if [[ $? -ne 0 ]]; then
-#    failed=true
-#fi
-
-if [[ $failed == "true" ]]; then
-    exit 1
-fi
+exit $?
